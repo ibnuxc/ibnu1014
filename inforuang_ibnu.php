f4d3fb2b304249b096601c3cc54dcff1db01dbd2
ibnuxc <54925634+ibnuxc@users.noreply.github.com>
2019-09-05 11:17:00 +0700

    Add files via upload

---
+++ inforuang_ibnu.php
@@ -0,0 +1,27 @@
+<?php
+header('Content-type: application/xml');
+header("Access-Control-Allow-Origin:*");
+
+echo "<inforuang>";
+
+	$host="https://github.com/ibnuxc/ibnu1014/blob/master/inforuang_ibnu.php";
+    $user="root";
+    $passwd="";
+    $db="inforuang_ibnu";
+    $koneksi = mysqli_connect($host, $user, $passwd)or die (mysqli_error($koneksi));
+    if ($koneksi)
+    {
+        mysqli_select_db($koneksi,$db) or die(mysql_error());
+    }
+	
+$sql="select * from ruang";
+$query=mysqli_query($koneksi, $sql) or die(mysqli_error($koneksi));
+while ($infoRuang=mysqli_fetch_array($query)) {
+	echo "<kode_ruangan kode_ruang='".$infoRuang['kode_ruang']."'>";
+	echo "<kode_ruang>".$infoRuang['kode_ruang']."</kode_ruang>";
+	echo "<nama_ruang>".$infoRuang['nama_ruang']."</nama_ruang>";
+	echo "<kapasitas>".$infoRuang['kapasitas']."</kapasitas>";
+	echo "<lokasi>".$infoRuang['lokasi']."</lokasi>";
+	echo "</kode_ruangan>";
+}
+echo "</inforuang>";
